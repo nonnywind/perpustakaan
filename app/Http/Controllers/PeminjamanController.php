463ca45e20133f1a969b9889b0dbdb51b3a77d2f
nonnywind <nonnywindarti@gmail.com>
2020-08-11 14:12:33 +0700

    Membuat stock berkurang setelah dipinjam

--- app/Http/Controllers/PeminjamanController.php
+++ app/Http/Controllers/PeminjamanController.php
@@ -17,6 +17,14 @@ class PeminjamanController extends Controller
                 'created_at' => date('Y-m-d H:i:s')
             ]);
 
+            $buku = \DB::table('m_buku')->where('id', $id)->first();
+            $qty_now = $buku->stock;
+            $qty_new = $qty_now - 1;
+
+            \DB::table('m_buku')->where('id', $id)->update([
+                'stock' => $qty_new
+            ]);
+
             \Session::flash('sukses', 'Buku berhasil dipinjam');
 
             return redirect('master/buku');
