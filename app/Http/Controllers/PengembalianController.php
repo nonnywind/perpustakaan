021090c3bd7d2faf6267d13c8705be5d8fbb8cbf
nonnywind <nonnywindarti@gmail.com>
2020-08-15 18:46:48 +0700

    Ketika pengembalian buku maka stock akan bertambah

--- app/Http/Controllers/PengembalianController.php
+++ app/Http/Controllers/PengembalianController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Peminjaman;
+use App\Models\M_buku;
 
 class PengembalianController extends Controller
 {
@@ -18,8 +19,20 @@ class PengembalianController extends Controller
 
     public function pengembalian($id)
     {
+        $dt = Peminjaman::find($id);
+        $id_buku = $dt->buku;
+
+        $buku = M_buku::find($id_buku);
+
+        $now = $buku->stock;
+        $stock_pengembalian = $now + 1;
+
         Peminjaman::where('id', $id)->update([
             'status' => 3
+        ]);
+
+        M_buku::where('id', $id)->update([
+            'stock' => $stock_pengembalian
         ]);
 
         return redirect('pengembalian-buku');
